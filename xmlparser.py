9cedf6cc9a723d7276a1aec9b60ad01f5f7087c4
Adrian Aguilar <adrian11_12@hotmail.com>
2014-01-26 22:54:30 -0500

    lista preparada para agregar en estructura
    lista preparada para agregar en estructura y creación de función que
    quita el encabezado

--- xmlparser.py
+++ xmlparser.py
@@ -1,51 +1,76 @@
-class Device:
-	def __init__ (self,id_device,user_agent,fall_back):
-		self.id = id_device
-		self.user = user_agent
-		self.fall = fall_back
+'''
+Created on 26/01/2014
 
-	def impridevice (self):
-                print ('Device: id='+self.id+' user_agent='+self.user+' fall_back='+self.fall)
+@author: adrian
+'''
+
+class Device:
+    def __init__ (self,id_device,user_agent,fall_back):
+        self.id = id_device
+        self.user = user_agent
+        self.fall = fall_back
+
+    def impridevice (self):
+                print 'Device: id='+self.id+' user_agent='+self.user+' fall_back='+self.fall
 
 class Group:
-	def __init__ (self,id_group):
-		self.id = id_group
+    def __init__ (self,id_group):
+        self.id = id_group
 
-	def imprigroup (self):
-                print ('Group: id='+self.id)
-	
+    def imprigroup (self):
+                print 'Group: id='+self.id
+    
 class Capability:
-	def __init__ (self,name,value):
-		self.name = name
-		self.value = value
+    def __init__ (self,name,value):
+        self.name = name
+        self.value = value
 
-	def impricapability (self):
-                print ('Capability: name='+self.name+' value='+self.value)
+    def impricapability (self):
+                print 'Capability: name='+self.name+' value='+self.value
 
 class Grupo:
-	def __init__ (self,device,group,capability):
-		self.device = device
-		self.group = group
-		self.capability = capability
+    def __init__ (self,device,group,capability):
+        self.device = device
+        self.group = group
+        self.capability = capability
+        
+def quitarBasura1(lines):
+        newlines = []
+        for str in lines:
+                newline = str.translate(None,'""')
+                newlines.append(newline)
+        return newlines
+
 
 def quitarBasura(lines):
         newlines = []
         for str in lines:
-                newline = str.translate(None,"<>=/\" ")
-                newlines.append(newline)
+                newline = str.replace('<',' ').replace('>',' ').replace('=',' ').replace('/',' ').split()
+                #newline = str.translate(None,'<>=/\""')
+                newlines.append(quitarBasura1(newline))
         return newlines
-		
+    
+    
+    
+    
+def quitarencabezado(lines):
+        newl = []
+        a=0
+        for str in lines:
+                if '<devices>' in str:
+                    a=1
+                if a==1:
+                    newl.append(str)
+        return newl
+        
 with open('test1.xml') as f:
         lines = f.read().splitlines()
-lista = quitarBasura(lines)
-
-for line in lista:
-	print line
-
-
-
+l=quitarencabezado(lines)
+lista = quitarBasura(l)
+print lista
 print "Archivo cargado."
 """
+
 a = Device ("id dev","user dev","fall dev")
 a.impridevice()
 b = Group ("id group")
